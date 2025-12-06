225c429e7df2314bb87e13d7b412b175bab6b391
isaak146-dls <isaakmonroy2@protonmail.com>
2025-12-06 01:11:36 -0600

    Update webhook URL and maintain functionality

--- bot_candado_siempre.py
+++ bot_candado_siempre.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta # <--- Importamos timedelta
 
 # --- CONFIGURACIÓN ---
 LISTA_USUARIOS = ["m0ritaav", "fresaskoncremq", "yazminsitq", "exorcismxq", "jerezanotravis"]
-WEBHOOK_URL = "TU_WEBHOOK_AQUI" # <--- ¡PON TU WEBHOOK!
+WEBHOOK_URL = "https://discord.com/api/webhooks/1446757512081707071/SKZzU2b3RHs-yz3g6iTOonfIz9SR-ZTd04sjCPeJ4uQ5oTG5SqGMtXv-7s09XoCxwyap" # <--- ¡PON TU WEBHOOK!
 
 # Usamos Pixwox
 BASE_URL = "https://www.pixwox.com/profile/{}/"
@@ -112,3 +112,4 @@ for usuario in LISTA_USUARIOS:
 
 guardar_bd(base_datos)
 print("--- Fin ---")
+
